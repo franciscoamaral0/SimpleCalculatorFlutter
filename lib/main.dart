c022ac6c289d254e9664533e02ca7c5f8cc06d07
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-06-25 20:12:03 +0100

    add buttons on calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:calculator_project/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'display.dart';
+import 'number_button.dart';
+
 const appName = "Simple Calculator";
 void main() => runApp(const App());
 
@@ -21,13 +24,42 @@ class App extends StatelessWidget {
   }
 }
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String display = "0";
+  String firstNumber = "";
+  void insert(String char) {
+    firstNumber += char;
+    setState(() {
+      display = firstNumber;
+    });
+  }
+
+  void clearCalculator() {
+    firstNumber = "";
+    setState(() {
+      display = firstNumber;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 70.0),
+        child: FloatingActionButton(
+          onPressed: clearCalculator,
+          child: const Text("C"),
+        ),
+      ),
       // backgroundColor: theme.colorScheme.primary,
       appBar: AppBar(
         title: const Text(
@@ -36,18 +68,52 @@ class Calculator extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
+        children: [
           Expanded(
             flex: 3,
-            child: Card(
-              elevation: 5,
-              child: Text("data"),
+            child: Display(
+              display: display,
             ),
           ),
-          Expanded(child: Text("First line")),
-          Expanded(child: Text("Secondary line")),
-          Expanded(child: Text("Third line")),
-          Expanded(child: Text("Fourth Line")),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                NumberButton(number: "7", onNumberPressed: insert),
+                NumberButton(number: "8", onNumberPressed: insert),
+                NumberButton(number: "9", onNumberPressed: insert),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                NumberButton(number: "6", onNumberPressed: insert),
+                NumberButton(number: "5", onNumberPressed: insert),
+                NumberButton(number: "4", onNumberPressed: insert),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                NumberButton(number: "3", onNumberPressed: insert),
+                NumberButton(number: "2", onNumberPressed: insert),
+                NumberButton(number: "1", onNumberPressed: insert),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // NumberButton(number: "0", onNumberPressed: insert),
+                // NumberButton(number: ",", onNumberPressed: insert),
+              ],
+            ),
+          ),
         ],
       ),
     );
